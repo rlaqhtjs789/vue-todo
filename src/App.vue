18678e4d77065be73b537008094fbe5b7de3ef7f
rlaqhtjs789 <rlaqhtjs789@naver.com>
2022-09-30 15:36:19 +0900

    change output directory

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>vue-TodoList</h1>
+    <input 
+      type="text" 
+      @keyup.enter="addTodo"
+      v-model="todoText"
+    />
+
+    <hr>
+
+    <todoList 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :todo="todo" 
+      @todo-Check="checkChange"
+      @todo-del="todoDel"
+    />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import todoList from './components/todoList.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    todoList
+  },
+  data() { 
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: "text1", checked: false },
+        { id: 2, text: "text2", checked: false }
+      ]
+    }
+  },
+  methods: {
+    addTodo(e){
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+
+      this.todoText = '';
+    },
+    checkChange({id, checked}){
+      const index = this.todos.findIndex(todo => {
+        return todo.id == id;
+      });
+
+      this.todos[index].checked = checked;
+    },
+    todoDel({id}){
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id == id;
+      // });
+
+      // this.todos.splice(index, 1);
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
 
 <style>
+body{
+  background: mintcream;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 500px;
+  padding: 10px 20px 50px 20px;
+  margin: 0 auto;
+  background: #fff;
+}
+#app>input{
+  width: 50%;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="todoList">
+        <input 
+            type="checkbox"
+            @click="todoCheck"
+        />
+        <span
+            :style="todo.checked ? 'text-decoration:line-through' : ''"
+        >{{ todo.text }}</span>
+        <button @click="todoDelete">제거</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        todoCheck(e){
+            this.$emit("todo-Check",{
+                id: this.todo.id,
+                checked: e.target.checked
+            });
+        },
+        todoDelete(){
+            this.$emit("todo-del", {
+                id: this.todo.id
+            });
+        }
+    }
+}
+</script>
+
+<style>
+    .todoList{display: flex; align-items: center; width: 100%; height: 50px; position: relative;}
+    .todoList>span{display: inline-block; margin-left: 6px;}
+    .todoList>button{position: absolute; right: 0; top: 50%; transform: translateY(-50%); background: orange; border: 0; color: #fff; font-size: 13px; font-weight: bold; padding: 5px 15px; border-radius: 5px; cursor: pointer;}
+</style>
